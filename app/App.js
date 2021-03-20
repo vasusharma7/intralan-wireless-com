20cda27a827e059110444add45f49f442723d0ed
vasusharma7 <vasusharma2017@outlook.com>
2021-03-21 00:18:55 +0530

    feat: connection search and connect working

--- app/App.js
+++ app/App.js
@@ -9,58 +9,81 @@ export default function App() {
   const [info, setInfo] = useState({});
   const [search, setSearch] = useState(true);
   const [interval, assignInterval] = useState(-1);
-  const [block, setBlock] = useState(new Netmask("192.168.1.0/25"));
-  const [waitTime, setWaitTime] = useState(500);
+  const [block, setBlock] = useState("192.168.1.0/24");
+  const [ips, setIps] = useState([]);
+  const [waitTime, setWaitTime] = useState(1000);
+
+  useEffect(() => {
+    const generator = new Netmask(block);
+    const collector = [];
+    generator.forEach((ip) => collector.push(ip));
+    setIps(collector);
+  }, [block]);
+
   useEffect(() => {
     console.log("connections found : ", Object.keys(connections).length);
+    for (let ip in connections) {
+      if (!info[ip])
+        connections[ip].on("broadcast", (data) => {
+          console.log("receiving broadcast data", data);
+          setInfo({ ...info, ip: data });
+          connections[ip].off("broadcast");
+        });
+    }
   }, [connections]);
+
   useEffect(() => {
     console.log("info found : ", Object.keys(info).length);
   }, [info]);
 
-  const startSearch = async (e) => {
-    e.preventDefault();
-    Alert.alert(`wait time is  ${waitTime}`);
-    // let int = setTimeout(async () => {
-    if (!search) {
-      clearInterval(interval);
-      assignInterval(-1);
-      return;
+  const sleep = (milliseconds) => {
+    let timeStart = new Date().getTime();
+    while (true) {
+      let elapsedTime = new Date().getTime() - timeStart;
+      if (elapsedTime > milliseconds) {
+        break;
+      }
     }
-    // if (interval == -1) assignInterval(int);
-    console.log("starting searching....");
-    await new Promise((resolve, reject) => {
-      block.forEach(async (ip, long, index) => {
-        const socket = await socketIOClient(`http://${ip}:5000`);
-        setTimeout(() => {
-          console.log(socket.connected, ip);
-          if (socket.connected && !Object.keys(connections).includes(ip)) {
-            console.log(ip, socket.connected);
-            socket.on("broadcast", (data) => {
-              setConnections({ ...connections, ip: socket });
-              console.log("receiving broadcast data", data);
-              setInfo({ ...info, ip: data });
-              socket.off("broadcast");
-            });
-          }
-          if (ip === block.last) {
-            resolve();
-          }
-        }, waitTime);
-      });
-    }).then(() => {
-      console.log("search complete");
-    });
-    // }, 0);
   };
-  useEffect(() => {});
 
+  const connect = async (ip) => {
+    return new Promise(async (resolve, reject) => {
+      const socket = await socketIOClient(`http://${ip}:5000`);
+      socket.on("connect", () => {
+        console.log(socket.id, socket.connected);
+        setConnections({ ...connections, ip: socket });
+      });
+      resolve(ip);
+    });
+  };
+
+  const startSearch = async () => {
+    if (!ips.length) return;
+    let int = setInterval(async () => {
+      if (!search) {
+        clearInterval(interval);
+        assignInterval(-1);
+        return;
+      }
+      if (interval == -1) assignInterval(int);
+      console.log("starting searching....", ips.length);
+      await Promise.all(ips.map(async (ip) => await connect(ip)))
+        .then((res) => console.log(res.length))
+        .catch((ips) => console.log(ips));
+    }, 5000);
+  };
+  useEffect(() => {
+    if (interval !== -1) clearInterval(interval);
+    startSearch();
+  }, [ips]);
+  useEffect(() => {
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Button onPress={startSearch} title="Search">
-        SEARCH
-      </Button>
     </View>
   );
 }
