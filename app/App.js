f3839bb0b4147f1421c972562a16f3d27dccb415
vasusharma7 <vasusharma2017@outlook.com>
2021-03-20 18:42:03 +0530

    searching and connection: partially working for lower subnets

--- app/App.js
+++ app/App.js
@@ -1,12 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
+const socketIOClient = require("socket.io-client");
+const Netmask = require("netmask").Netmask;
 
 export default function App() {
+  const [connections, setConnections] = useState({});
+  const [info, setInfo] = useState({});
+  const [search, setSearch] = useState(true);
+  const [interval, assignInterval] = useState(-1);
+  const [block, setBlock] = useState(new Netmask("192.168.1.0/25"));
+  const [waitTime, setWaitTime] = useState(500);
+  useEffect(() => {
+    console.log("connections found : ", Object.keys(connections).length);
+  }, [connections]);
+  useEffect(() => {
+    console.log("info found : ", Object.keys(info).length);
+  }, [info]);
+
+  const startSearch = async (e) => {
+    e.preventDefault();
+    Alert.alert(`wait time is  ${waitTime}`);
+    // let int = setTimeout(async () => {
+    if (!search) {
+      clearInterval(interval);
+      assignInterval(-1);
+      return;
+    }
+    // if (interval == -1) assignInterval(int);
+    console.log("starting searching....");
+    await new Promise((resolve, reject) => {
+      block.forEach(async (ip, long, index) => {
+        const socket = await socketIOClient(`http://${ip}:5000`);
+        setTimeout(() => {
+          console.log(socket.connected, ip);
+          if (socket.connected && !Object.keys(connections).includes(ip)) {
+            console.log(ip, socket.connected);
+            socket.on("broadcast", (data) => {
+              setConnections({ ...connections, ip: socket });
+              console.log("receiving broadcast data", data);
+              setInfo({ ...info, ip: data });
+              socket.off("broadcast");
+            });
+          }
+          if (ip === block.last) {
+            resolve();
+          }
+        }, waitTime);
+      });
+    }).then(() => {
+      console.log("search complete");
+    });
+    // }, 0);
+  };
+  useEffect(() => {});
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button onPress={startSearch} title="Search">
+        SEARCH
+      </Button>
     </View>
   );
 }
@@ -14,8 +68,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ broadcast.js
@@ -0,0 +1,30 @@
+const cors = require("cors");
+
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json({ limit: "50mb" }));
+app.use(cors());
+app.options("*", cors());
+const http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+//io.origins('*//*:*');
+const emit = setInterval(() => {
+  io.emit("broadcast", { ip: "10.1.1.1", port: 12 });
+}, 10);
+io.sockets.on("connection", (client) => {
+  //setTimeout(() => clearInterval(emit), 1000);
+
+  io.emit("broadcast", { ip: "10.1.1.1", port: 12 });
+  console.log(client.id);
+});
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+http.listen(port, () => {
+  console.log(`Broadcasting on port ${port}`);
+});
