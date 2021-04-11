27eb32fcab64b49bf8a30d061b7b41f7aabb1bc9
vasusharma7 <vasusharma2017@outlook.com>
2021-04-11 21:58:45 +0530

    changed to rn-fetch-blob

--- native-app/android/app/src/main/java/com/vasusharma7/intralancom/MainApplication.java
+++ native-app/android/app/src/main/java/com/vasusharma7/intralancom/MainApplication.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.RNFetchBlob.RNFetchBlobPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.pusherman.networkinfo.RNNetworkInfoPackage;
